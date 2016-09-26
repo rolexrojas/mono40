998a19ae34cbb9e14e0e157643a31abac796818a
Hector Vasquez <hecvasro@gmail.com>
2016-09-26 17:23:47 -0400

    Adds digit and delete pin pad buttons processing

--- app/src/main/java/com/gbh/movil/ui/main/pin/PinConfirmationFragment.java
+++ app/src/main/java/com/gbh/movil/ui/main/pin/PinConfirmationFragment.java
@@ -7,8 +7,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.gbh.movil.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.BindViews;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+import timber.log.Timber;
 
 /**
  * TODO
@@ -16,6 +27,102 @@ import com.gbh.movil.R;
  * @author hecvasro
  */
 public class PinConfirmationFragment extends Fragment {
+  /**
+   * TODO
+   */
+  private static final long PIN_DOT_ANIMATION_DURATION = 75L;
+
+  /**
+   * TODO
+   */
+  private static final int SELECTED_DIGITS_LIMIT = 4;
+
+  /**
+   * TODO
+   */
+  private final List<Integer> selectedDigits = new ArrayList<>();
+
+  /**
+   * TODO
+   */
+  private Unbinder unbinder;
+
+  /**
+   * TODO
+   */
+  @BindViews({ R.id.pin_dot_first, R.id.pin_dot_second, R.id.pin_dot_third, R.id.pin_dot_fourth })
+  List<View> pinDots;
+
+  /**
+   * TODO
+   */
+  @BindViews({ R.id.pin_pad_button_zero, R.id.pin_pad_button_one, R.id.pin_pad_button_two,
+    R.id.pin_pad_button_three, R.id.pin_pad_button_four, R.id.pin_pad_button_five,
+    R.id.pin_pad_button_six, R.id.pin_pad_button_seven, R.id.pin_pad_button_eight,
+    R.id.pin_pad_button_nine })
+  List<View> digitPinPadButtons;
+
+  /**
+   * TODO
+   */
+  @BindView(R.id.pin_pad_button_delete)
+  View deletePinPadButton;
+
+  /**
+   * TODO
+   *
+   * @param index
+   *   TODO
+   * @param show
+   *   TODO
+   */
+  private void updatePinDot(final int index, final boolean show) {
+    if (index >= 0 && index < pinDots.size()) {
+      final View dot = pinDots.get(index);
+      dot.postDelayed(new Runnable() {
+        @Override
+        public void run() {
+          dot.setVisibility(show ? View.VISIBLE : View.GONE);
+        }
+      }, PIN_DOT_ANIMATION_DURATION);
+    }
+  }
+
+  /**
+   * TODO
+   *
+   * @param digitPinPadButton
+   *   TODO
+   */
+  @OnClick({ R.id.pin_pad_button_zero, R.id.pin_pad_button_one, R.id.pin_pad_button_two,
+    R.id.pin_pad_button_three, R.id.pin_pad_button_four, R.id.pin_pad_button_five,
+    R.id.pin_pad_button_six, R.id.pin_pad_button_seven, R.id.pin_pad_button_eight,
+    R.id.pin_pad_button_nine })
+  void onDigitPinPadButtonClicked(@NonNull TextView digitPinPadButton) {
+    final int digit = Integer.parseInt(digitPinPadButton.getText().toString());
+    Timber.d("Pin pad digit (%1$d) button clicked", digit);
+    if (selectedDigits.size() < SELECTED_DIGITS_LIMIT) {
+      selectedDigits.add(digit);
+      updatePinDot(selectedDigits.size() - 1, true);
+      if (selectedDigits.size() == SELECTED_DIGITS_LIMIT) {
+        // TODO: Start PIN confirmation.
+      }
+    }
+  }
+
+  /**
+   * TODO
+   */
+  @OnClick(R.id.pin_pad_button_delete)
+  void onDeletePinPadButtonClicked() {
+    Timber.d("Pin pad delete button clicked");
+    final int index = selectedDigits.size() - 1;
+    if (index >= 0) {
+      selectedDigits.remove(index);
+      updatePinDot(index, false);
+    }
+  }
+
   @Nullable
   @Override
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
@@ -23,10 +130,25 @@ public class PinConfirmationFragment extends Fragment {
     return inflater.inflate(R.layout.fragment_pin_confirmation, container, false);
   }
 
+  @Override
+  public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    // Binds all the annotated views and methods.
+    unbinder = ButterKnife.bind(this, view);
+  }
+
+  @Override
+  public void onDestroyView() {
+    super.onDestroyView();
+    // Unbinds all the annotated views and methods.
+    unbinder.unbind();
+  }
+
   /**
    * TODO
    *
-   * @param correctly TODO
+   * @param correctly
+   *   TODO
    */
   public final void dismiss(boolean correctly) {
     // TODO
