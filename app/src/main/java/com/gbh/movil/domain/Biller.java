7121282c9e2ddfef8eb4004749bf13a4c31b49dd
Hector Vasquez <hecvasro@gmail.com>
2016-11-02 11:27:42 -0400

    Adds the biller representation

---
+++ app/src/main/java/com/gbh/movil/domain/Biller.java
@@ -0,0 +1,68 @@
+package com.gbh.movil.domain;
+
+import android.support.annotation.NonNull;
+
+/**
+ * Biller representation.
+ *
+ * @author hecvasro
+ */
+public class Biller {
+  /**
+   * Biller's identifier.
+   */
+  private final String id;
+  /**
+   * Biller's name.
+   */
+  private final String name;
+
+  /**
+   * Constructs a new biller.
+   *
+   * @param id
+   *   Biller's identifier.
+   * @param name
+   *   Biller's name.
+   */
+  public Biller(@NonNull String id, @NonNull String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  /**
+   * Gets the identifier of the biller.
+   *
+   * @return Biller's identifier.
+   */
+  @NonNull
+  public final String getId() {
+    return id;
+  }
+
+  /**
+   * Gets the name of the biller.
+   *
+   * @return Biller's name.
+   */
+  @NonNull
+  public final String getName() {
+    return name;
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    return super.equals(object) || (object != null && object instanceof Biller
+      && ((Biller) object).id.equals(id));
+  }
+
+  @Override
+  public int hashCode() {
+    return id.hashCode();
+  }
+
+  @Override
+  public String toString() {
+    return Biller.class.getSimpleName() + ":{id='" + id + ",name='" + name + "'}";
+  }
+}
