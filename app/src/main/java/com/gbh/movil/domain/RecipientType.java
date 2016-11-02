828f5e378319abb12fecfe7eeae0e84378d8dc7d
Hector Vasquez <hecvasro@gmail.com>
2016-11-02 11:39:05 -0400

    Renames the recipient type CONTRACT to INVOICE

--- app/src/main/java/com/gbh/movil/domain/RecipientType.java
+++ app/src/main/java/com/gbh/movil/domain/RecipientType.java
@@ -11,9 +11,9 @@ import java.lang.annotation.RetentionPolicy;
  * @author hecvasro
  */
 @Retention(RetentionPolicy.SOURCE)
-@IntDef({ RecipientType.CONTACT, RecipientType.CONTRACT, RecipientType.CREDIT_CARD})
+@IntDef({ RecipientType.CONTACT, RecipientType.INVOICE, RecipientType.CREDIT_CARD})
 public @interface RecipientType {
   int CONTACT = 0;
-  int CONTRACT = 1;
+  int INVOICE = 1;
   int CREDIT_CARD = 2;
 }
