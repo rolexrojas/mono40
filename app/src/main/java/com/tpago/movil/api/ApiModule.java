cee6790f1d45997d8dc37ac0dc5034c5d9146951
Hector Vasquez <hecvasro@gmail.com>
2017-03-27 16:22:50 -0400

    Upgrades Retrofit's version

--- app/src/main/java/com/tpago/movil/api/ApiModule.java
+++ app/src/main/java/com/tpago/movil/api/ApiModule.java
@@ -1,10 +1,36 @@
 package com.tpago.movil.api;
 
+import com.google.gson.Gson;
+import com.tpago.movil.app.DeviceManager;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
+import dagger.Provides;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * @author hecvasro
  */
-@Module(includes = ApiFlavorModule.class)
+@Module(includes = ApiUrlFlavorModule.class)
 public class ApiModule {
+  @Provides
+  @Singleton
+  ApiBridge provideApiBridge(
+    DeviceManager deviceManager,
+    Gson gson,
+    HttpUrl baseUrl,
+    OkHttpClient httpClient) {
+    final Retrofit retrofit = new Retrofit.Builder()
+      .baseUrl(baseUrl)
+      .client(httpClient)
+      .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+      .addConverterFactory(GsonConverterFactory.create(gson))
+      .build();
+    return new RetrofitApiBridge(deviceManager, retrofit);
+  }
 }
