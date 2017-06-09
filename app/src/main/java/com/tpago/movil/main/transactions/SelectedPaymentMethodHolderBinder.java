7221fee338326dd3eb4d5599f76d942440f065a0
Hector Vasquez <h.vasquez@gbh.com.do>
2017-06-09 04:12:07 -0400

    Removes the word 'Desde' from all payment method choosers

--- app/src/main/java/com/tpago/movil/main/transactions/SelectedPaymentMethodHolderBinder.java
+++ app/src/main/java/com/tpago/movil/main/transactions/SelectedPaymentMethodHolderBinder.java
@@ -2,9 +2,6 @@ package com.tpago.movil.main.transactions;
 
 import android.content.Context;
 
-import com.tpago.movil.R;
-import com.tpago.movil.d.domain.Product;
-
 /**
  * @author hecvasro
  */
@@ -12,10 +9,5 @@ final class SelectedPaymentMethodHolderBinder
   extends BasePaymentMethodHolderBinder<SelectedPaymentMethodHolder> {
   SelectedPaymentMethodHolderBinder(Context context) {
     super(context);
-  }
-
-  @Override
-  protected String formatIdentifier(Product product) {
-    return getString(R.string.from) + " " + super.formatIdentifier(product);
   }
 }
