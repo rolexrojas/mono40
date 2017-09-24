16d832c4346bdab3a35748dcfa6c1795fd329d76
Hector Vasquez <hecvasro@gmail.com>
2017-09-24 09:41:10 -0400

    Resolve https://jira.gcs-systems.com:8080/browse/APTPII-150

--- app/src/main/java/com/tpago/movil/d/domain/PaymentResult.java
+++ app/src/main/java/com/tpago/movil/d/domain/PaymentResult.java
@@ -1,19 +1,35 @@
 package com.tpago.movil.d.domain;
 
 import com.google.auto.value.AutoValue;
+import com.google.auto.value.extension.memoized.Memoized;
 import com.google.gson.Gson;
 import com.google.gson.TypeAdapter;
 import com.google.gson.annotations.SerializedName;
+import com.tpago.movil.DigitHelper;
 
 /**
  * @author Hector Vasquez
  */
 @AutoValue
 public abstract class PaymentResult {
+
   public static TypeAdapter<PaymentResult> typeAdapter(Gson gson) {
     return new AutoValue_PaymentResult.GsonTypeAdapter(gson);
   }
 
-  @SerializedName("transaction-id") public abstract String id();
-  @SerializedName("transaction-message") public abstract String message();
+  @SerializedName("transaction-id")
+  abstract String internalId();
+
+  @Memoized
+  public String id() {
+    return DigitHelper.removeNonDigits(this.internalId());
+  }
+
+  @SerializedName("transaction-message")
+  abstract String internalMessage();
+
+  @Memoized
+  public String message() {
+    return this.internalMessage();
+  }
 }
