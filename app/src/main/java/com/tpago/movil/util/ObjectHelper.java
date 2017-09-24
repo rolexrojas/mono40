db0cd9e752f54086463e96ac1b1c9ef6b6545376
Hector Vasquez <hecvasro@gmail.com>
2017-09-24 09:26:59 -0400

    Update the ObjectHelper class
    A new method was added, it checks if an object is null and throws
    a NullPointerException if it is.

--- app/src/main/java/com/tpago/movil/util/ObjectHelper.java
+++ app/src/main/java/com/tpago/movil/util/ObjectHelper.java
@@ -1,5 +1,7 @@
 package com.tpago.movil.util;
 
+import static java.lang.String.format;
+
 /**
  * Collection of helpers for {@link Object objects}.
  *
@@ -8,7 +10,7 @@ package com.tpago.movil.util;
 public final class ObjectHelper {
 
   /**
-   * Checks if the given {@link Object object} is null or not.
+   * Checks whether the given {@link Object object} is null or not.
    *
    * @param object
    *   {@link Object} that will be checked.
@@ -20,7 +22,7 @@ public final class ObjectHelper {
   }
 
   /**
-   * Checks if the given {@link Object object} is null.
+   * Checks whether the given {@link Object object} is null.
    *
    * @param object
    *   {@link Object} that will be checked.
@@ -29,6 +31,30 @@ public final class ObjectHelper {
    */
   public static boolean isNotNull(Object object) {
     return !isNull(object);
+  }
+
+  /**
+   * Checks if the given {@link T object} is null. An {@link NullPointerException exception} will be
+   * thrown if it's null.
+   *
+   * @param object
+   *   {@link T Object} that will be checked.
+   * @param argumentName
+   *   {@link String} that will be used for the message of the {@link NullPointerException
+   *   exception}.
+   * @param <T>
+   *   Type of object that will be checked.
+   *
+   * @return Given {@link T object} if it isn't null.
+   *
+   * @throws NullPointerException
+   *   If {@code object} is null.
+   */
+  public static <T> T checkNotNull(T object, String argumentName) {
+    if (isNull(object)) {
+      throw new NullPointerException(format("isNull(%1$s)", argumentName));
+    }
+    return object;
   }
 
   private ObjectHelper() {
