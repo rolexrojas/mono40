17f9f1e1a924d3deb1cfb6c1921015bb76361aba
Hector Vasquez <hecvasro@gmail.com>
2017-11-28 11:28:33 -0400

    Fix the build process for the pre-production environment

---
+++ app/src/preproduction/java/com/tpago/movil/dep/api/ApiUrlFlavorModule.java
@@ -0,0 +1,19 @@
+package com.tpago.movil.dep.api;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import okhttp3.HttpUrl;
+
+/**
+ * @author hecvasro
+ */
+@Module
+public final class ApiUrlFlavorModule {
+  @Provides
+  @Singleton
+  HttpUrl provideBaseUrl() {
+    return HttpUrl.parse("https://tpagonet.gcs-systems.com/api/neo/");
+  }
+}
