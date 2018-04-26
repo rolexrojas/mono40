357919369c6ae2d738626a81f08a5defc8ef56a4
Hector Vasquez <hecvasro@gmail.com>
2018-04-26 17:25:39 -0400

    Improve documentation of 'PosServiceFactory'

--- app/src/main/java/com/tpago/movil/purchase/PosServiceFactory.java
+++ app/src/main/java/com/tpago/movil/purchase/PosServiceFactory.java
@@ -3,7 +3,10 @@ package com.tpago.movil.purchase;
 import android.support.annotation.Nullable;
 
 /**
- * Factory of {@link PosService}
+ * Factory responsible for creating {@link PosService} instances
+ * <p>
+ * {@link PosService} instances will only be created if there is an {@link android.nfc.NfcAdapter}
+ * available.
  */
 public interface PosServiceFactory {
 
