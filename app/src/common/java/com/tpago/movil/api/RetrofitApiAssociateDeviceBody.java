fbfdc24d063c9345aee99690ce3ff3808098511b
Chris Gomez <c.gomez@gbh.com.do>
2019-06-13 17:54:28 -0400

    feature: device check android

--- app/src/common/java/com/tpago/movil/api/RetrofitApiAssociateDeviceBody.java
+++ app/src/common/java/com/tpago/movil/api/RetrofitApiAssociateDeviceBody.java
@@ -6,6 +6,7 @@ import com.google.gson.Gson;
 import com.google.gson.TypeAdapter;
 import com.google.gson.annotations.SerializedName;
 import com.tpago.movil.Email;
+import com.tpago.movil.data.api.DeviceInformationBody;
 import com.tpago.movil.lib.Password;
 import com.tpago.movil.PhoneNumber;
 import com.tpago.movil.util.ObjectHelper;
@@ -39,35 +40,8 @@ public abstract class RetrofitApiAssociateDeviceBody {
   @SerializedName("new-imei")
   abstract String deviceId();
 
-  @SerializedName("manufacturer")
-  abstract String manufacturer();
-
-  @SerializedName("serial")
-  abstract String serial();
-
-  @SerializedName("fingerprint")
-  abstract String fingerprint();
-
-  @SerializedName("device")
-  abstract String device();
-
-  @SerializedName("display")
-  abstract String display();
-
-  @SerializedName("board")
-  abstract String board();
-
-  @SerializedName("bootloader")
-  abstract String bootloader();
-
-  @SerializedName("brand")
-  abstract String brand();
-
-  @SerializedName("hardware")
-  abstract String hardware();
-
-  @SerializedName("model")
-  abstract String model();
+  @SerializedName("device-informacion")
+  abstract DeviceInformationBody deviceInformationBody();
 
   @Memoized
   @Override
@@ -107,16 +81,7 @@ public abstract class RetrofitApiAssociateDeviceBody {
       return this.password(password.value());
     }
 
-    public abstract Builder manufacturer(String manufacturer);
-    public abstract Builder serial(String serial);
-    public abstract Builder fingerprint(String fingerprint);
-    public abstract Builder device(String device);
-    public abstract Builder display(String display);
-    public abstract Builder board(String board);
-    public abstract Builder bootloader(String bootloader);
-    public abstract Builder brand(String brand);
-    public abstract Builder hardware(String hardware);
-    public abstract Builder model(String model);
+    public abstract Builder deviceInformationBody(DeviceInformationBody deviceInformationBody);
 
     public abstract Builder deviceId(String deviceId);
 
--- app/src/common/java/com/tpago/movil/api/RetrofitApiSignUpBody.java
+++ app/src/common/java/com/tpago/movil/api/RetrofitApiSignUpBody.java
@@ -8,6 +8,7 @@ import com.google.gson.annotations.SerializedName;
 import com.tpago.movil.Code;
 import com.tpago.movil.Email;
 import com.tpago.movil.PhoneNumber;
+import com.tpago.movil.data.api.DeviceInformationBody;
 import com.tpago.movil.lib.Password;
 import com.tpago.movil.util.ObjectHelper;
 
@@ -49,38 +50,9 @@ public abstract class RetrofitApiSignUpBody {
     @SerializedName("pin")
     abstract String pin();
 
-    @SerializedName("imei")
-    abstract String deviceId();
+    @SerializedName("device-informacion")
+    abstract DeviceInformationBody deviceInformationBody();
 
-    @SerializedName("manufacturer")
-    abstract String manufacturer();
-
-    @SerializedName("serial")
-    abstract String serial();
-
-    @SerializedName("fingerprint")
-    abstract String fingerprint();
-
-    @SerializedName("device")
-    abstract String device();
-
-    @SerializedName("display")
-    abstract String display();
-
-    @SerializedName("board")
-    abstract String board();
-
-    @SerializedName("bootloader")
-    abstract String bootloader();
-
-    @SerializedName("brand")
-    abstract String brand();
-
-    @SerializedName("hardware")
-    abstract String hardware();
-
-    @SerializedName("model")
-    abstract String model();
 
     @Memoized
     @Override
@@ -136,18 +108,7 @@ public abstract class RetrofitApiSignUpBody {
             return this.pin(pin.value());
         }
 
-        public abstract Builder deviceId(String deviceId);
-
-        public abstract Builder manufacturer(String manufacturer);
-        public abstract Builder serial(String serial);
-        public abstract Builder fingerprint(String fingerprint);
-        public abstract Builder device(String device);
-        public abstract Builder display(String display);
-        public abstract Builder board(String board);
-        public abstract Builder bootloader(String bootloader);
-        public abstract Builder brand(String brand);
-        public abstract Builder hardware(String hardware);
-        public abstract Builder model(String model);
+        public abstract Builder deviceInformationBody(DeviceInformationBody deviceInformationBody);
 
         public abstract RetrofitApiSignUpBody build();
     }
